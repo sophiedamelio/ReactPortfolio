793e27357334c3580b92bb019c0fff4e9c234cf9
Sophie Damelio <sophiedamelio@Sophies-MacBook-Pro.local>
2022-04-10 18:43:09 -0700

    Still playing with layout

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,10 @@
+import "./Projects.css"
+
+
+export default function Projects() {
+	return (
+		<div id="projects">
+			<p>projects</p>
+		</div>
+	)
+}
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,12 @@
 import Header from '../../components/Header/Header'
+import Projects from '../../components/Projects/Projects'
 
 
 export default function HomePage() {
 	return (
-		<Header />
+		<div id="proj">
+			<Header className="components" />
+			<Projects className="components" />
+		</div>
 	)
 }
