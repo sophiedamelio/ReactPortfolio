5d1da21462ff5342629391bbd979ca2da891798e
sophiedamelio <sophiedamelio@gmail.com>
2022-12-24 17:28:54 -0800

    update links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,9 @@ import "./Footer.css"
 export default function Footer() {
 	return (
 		<>
-			<a name="contact-anchor"><h2>Contact</h2></a>
+			{/*<a name="contact-anchor">*/}
+			<h2>Contact</h2>
+			{/*</a>*/}
 			<p>Email: sophiedamelio@gmail.com</p>
 			<a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/sophiedamelio/"><button class="cta">LinkedIn</button></a>
 			<a target="_blank" rel="noopener noreferrer" href="https://github.com/sophiedamelio/"><button class="cta">GitHub</button></a>
