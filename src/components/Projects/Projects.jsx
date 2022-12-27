ecb085aacaba414c03f54b08c1c01fda5f2fd1ab
sophiedamelio <sophiedamelio@gmail.com>
2022-12-26 16:41:32 -0800

    work on colors and layout

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -35,8 +35,8 @@ export default function Projects({ sophie, setSophie }) {
 
 	return (
 		<div id="projects">
-			{/*<p>projects</p>
-			<button onClick={() => {
+			<h2>Projects</h2>
+			{/*<button onClick={() => {
 				if (sophie === "https://i.imgur.com/P5KDK68.png") {
 					setSophie("https://i.imgur.com/tCg6n3O.jpg")
 				} else {
@@ -44,49 +44,63 @@ export default function Projects({ sophie, setSophie }) {
 				}
 			}}
 			>CHANGE SOPHIE'S VALUE</button>
-			<img src={sophie} />
+			<img src={sophie} />*/}
 
 			{/* if listSkills is truthy (has value in this case), list them out as a unordered list. If listSkills is falsey/ does not have value, render nothing */}
 			{/*{listSkills ? <ul>{listSkills}</ul> : null}*/}
-			<h3>songSpace Express App</h3>
-			<div>
-				<img src="https://i.imgur.com/QmloUf2.png" alt="screenshot of app" />
-				<p>I built a Fullstack Express App with React frontend, that performs full CRUD functionality.
-					I utilized JWT for authentication, MongoDB for storage, Semantic-ui-react for styling, and Heroku for deployment.
-					To view the app use this login: username: demo@demo.com, password: demo</p>
-				<a target="_blank" rel="noopener noreferrer" href="https://github.com/sophiedamelio/project_4"><button>Link to the Repo</button></a>
-				<a target="_blank" rel="noopener noreferrer" href="https://songspace-app.herokuapp.com/"><button>Explore songSpace</button></a>
+			<div class="projectCard">
+				<div class="cardBackground">
+					<h3>songSpace Express App</h3>
+					<img src="https://i.imgur.com/QmloUf2.png" alt="screenshot of app" />
+					<p>I built a Fullstack Express App with React frontend, that performs full CRUD functionality.
+						I utilized JWT for authentication, MongoDB for storage, Semantic-ui-react for styling, and Heroku for deployment.
+						To view the app use this login: username: demo@demo.com, password: demo</p>
+					<a target="_blank" rel="noopener noreferrer" href="https://github.com/sophiedamelio/project_4"><button>Link to the Repo</button></a>
+					<a target="_blank" rel="noopener noreferrer" href="https://songspace-app.herokuapp.com/"><button>Explore songSpace</button></a>
+				</div>
 			</div>
-			<h3>everyJob Django App</h3>
-			<div >
-				<img src="https://i.imgur.com/ixSrCHd.png" alt="screenshot of app" />
-				<p class="card-text">As a part of a four person team I collaboratively  built a Fullstack Django app with full CRUD functionality.
-					We utilized PostgreSQL for database storage, Django built-in Authentication, and deployment on Heroku.</p>
-				<a target="_blank" rel="noopener noreferrer" href="https://github.com/sophiedamelio/everyJob"><button>Link to the Repo</button></a>
-				<a target="_blank" rel="noopener noreferrer" href="https://sophie-everyjob.herokuapp.com/"><button>Explore everyJob</button></a>
+
+			<div class="projectCard">
+				<div class="cardBackground">
+					<h3>everyJob Django App</h3>
+					<img src="https://i.imgur.com/ixSrCHd.png" alt="screenshot of app" />
+					<p class="card-text">As a part of a four person team I collaboratively  built a Fullstack Django app with full CRUD functionality.
+						We utilized PostgreSQL for database storage, Django built-in Authentication, and deployment on Heroku.</p>
+					<a target="_blank" rel="noopener noreferrer" href="https://github.com/sophiedamelio/everyJob"><button>Link to the Repo</button></a>
+					<a target="_blank" rel="noopener noreferrer" href="https://sophie-everyjob.herokuapp.com/"><button>Explore everyJob</button></a>
+				</div>
 			</div>
-			<h3>Wishlist Express App</h3>
-			<div>
-				<img src="https://i.imgur.com/DgzgQqh.png" alt="screenshot of app" />
-				<p class="card-text">I built a Wishlist app using Express with full CRUD functionality and Google OAuth.
-					MongoDB is used for storage, and it is deployed on Heroku.</p>
-				<a target="_blank" rel="noopener noreferrer" href="https://github.com/sophiedamelio/project_two"><button>Link to the Repo</button></a>
-				<a target="_blank" rel="noopener noreferrer" href="https://sophie-project-two.herokuapp.com/"><button>Get Listing</button></a>
+
+			<div class="projectCard">
+				<div class="cardBackground">
+					<h3>Wishlist Express App</h3>
+					<img src="https://i.imgur.com/DgzgQqh.png" alt="screenshot of app" />
+					<p class="card-text">I built a Wishlist app using Express with full CRUD functionality and Google OAuth.
+						MongoDB is used for storage, and it is deployed on Heroku.</p>
+					<a target="_blank" rel="noopener noreferrer" href="https://github.com/sophiedamelio/project_two"><button>Link to the Repo</button></a>
+					<a target="_blank" rel="noopener noreferrer" href="https://sophie-project-two.herokuapp.com/"><button>Get Listing</button></a>
+				</div>
 			</div>
-			<h3 >Connect Four Browser Game</h3>
-			<div>
-				<img src="https://i.imgur.com/b9LmOUu.png" alt="screenshot of app" />
-				<p>I built a Connect Four browser game using JavaScript and HTML/CSS.
-					I used a Trello board to plan, and deployed via Github Pages. </p>
-				<a target="_blank" rel="noopener noreferrer" href="https://github.com/sophiedamelio/connect_four/"><button>Link to the Repo</button></a>
-				<a target="_blank" rel="noopener noreferrer" href="https://sophiedamelio.github.io/connect_four/"><button>Play Connect Four</button></a>
+
+			<div class="projectCard">
+				<div class="cardBackground">
+					<h3 >Connect Four Browser Game</h3>
+					<img src="https://i.imgur.com/b9LmOUu.png" alt="screenshot of app" />
+					<p>I built a Connect Four browser game using JavaScript and HTML/CSS.
+						I used a Trello board to plan, and deployed via Github Pages. </p>
+					<a target="_blank" rel="noopener noreferrer" href="https://github.com/sophiedamelio/connect_four/"><button>Link to the Repo</button></a>
+					<a target="_blank" rel="noopener noreferrer" href="https://sophiedamelio.github.io/connect_four/"><button>Play Connect Four</button></a>
+				</div>
 			</div>
-			<h3>Ideas To Go Website</h3>
-			<div>
-				<img src="https://i.imgur.com/Zgn5lPV.jpg" alt="screenshot of app" />
-				<p class="card-text">I developed and designed this website with provided design assets. I organized content, converted graphics and created page layouts. I wrote responsive CSS including section transitions, CTAs, and general styling of elements beyond the built in Squarespace options. </p>
-				<a target="_blank" rel="noopener noreferrer" href='https://github.com/sophiedamelio/itg_css'><button>Link to the Repo</button></a>
-				<a href='/itg_detail'><button>View</button></a>
+
+			<div class="projectCard">
+				<div class="cardBackground">
+					<h3>Ideas To Go Website</h3>
+					<img src="https://i.imgur.com/Zgn5lPV.jpg" alt="screenshot of app" />
+					<p class="card-text">I developed and designed this website with provided design assets. I organized content, converted graphics and created page layouts. I wrote responsive CSS including section transitions, CTAs, and general styling of elements beyond the built in Squarespace options. </p>
+					<a target="_blank" rel="noopener noreferrer" href='https://github.com/sophiedamelio/itg_css'><button>Link to the Repo</button></a>
+					<a href='/itg_detail'><button>View</button></a>
+				</div>
 			</div>
 		</div>
 	)
