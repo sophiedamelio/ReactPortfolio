bf42ecb1b63fcba58ea1c0794c9513fabe9ca5b5
sophiedamelio <sophiedamelio@gmail.com>
2023-01-04 13:30:11 -0800

    first use of 'doubling' prop on grid for responsive, in projects section

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -36,7 +36,7 @@ export default function Projects({ sophie, setSophie }) {
 
 	return (
 		<div>
-			<Grid columns={2}>
+			<Grid doubling columns={2}>
 				{/*<button onClick={() => {
 				if (sophie === "https://i.imgur.com/P5KDK68.png") {
 					setSophie("https://i.imgur.com/tCg6n3O.jpg")
@@ -50,88 +50,88 @@ export default function Projects({ sophie, setSophie }) {
 				{/* if listSkills is truthy (has value in this case), list them out as a unordered list. If listSkills is falsey/ does not have value, render nothing */}
 				{/*{listSkills ? <ul>{listSkills}</ul> : null}*/}
 				<h2 style={{ marginLeft: "5vh", fontFamily: "OPTIAmway" }}>Projects</h2>
-				<Grid.Row>
-					<Grid.Column>
-						<Card style={{ backgroundColor: "rgb(234, 204, 203)", width: "90%", height: "100%", marginLeft: "5vh", marginRight: "0vh" }}>
-							<Card.Content>
-								<Card.Header style={{ color: "rgb(110, 87, 91)", fontFamily: "OPTIAmway" }}>songSpace Express App</Card.Header>
-								<Image src="https://i.imgur.com/QmloUf2.png" alt="screenshot of app" size={'big'} style={{ paddingTop: "2vh", paddingBottom: "2vh" }} />
-								<Card.Description style={{ color: "rgb(110, 87, 91)" }}>
-									I built a Fullstack Express App with React frontend, that performs full CRUD functionality.
-									I utilized JWT for authentication, MongoDB for storage, Semantic-ui-react for styling, and Heroku for deployment.
-									Create your own account if you'd like, or view a demo of app using this login: <br></br>
-									username: demo@demo.com <br></br>
-									password: demo <br></br>
-									<a target="_blank" rel="noopener noreferrer" href="https://github.com/sophiedamelio/project_4" className="projectCta" style={{ marginLeft: "0vh" }}>Link to the Repo</a>
-									<a target="_blank" rel="noopener noreferrer" href="https://songspace-app.herokuapp.com/" className="projectCta" style={{ marginLeft: "0vh" }}>Explore songSpace</a>
-								</Card.Description>
-							</Card.Content>
-						</Card>
-					</Grid.Column>
+				{/*<Grid.Row>*/}
+				<Grid.Column>
+					<Card style={{ backgroundColor: "rgb(234, 204, 203)", width: "90%", height: "100%", marginLeft: "5vh", marginRight: "0vh" }}>
+						<Card.Content>
+							<Card.Header style={{ color: "rgb(110, 87, 91)", fontFamily: "OPTIAmway" }}>songSpace Express App</Card.Header>
+							<Image src="https://i.imgur.com/QmloUf2.png" alt="screenshot of app" size={'big'} style={{ paddingTop: "2vh", paddingBottom: "2vh" }} />
+							<Card.Description style={{ color: "rgb(110, 87, 91)" }}>
+								I built a Fullstack Express App with React frontend, that performs full CRUD functionality.
+								I utilized JWT for authentication, MongoDB for storage, Semantic-ui-react for styling, and Heroku for deployment.
+								Create your own account if you'd like, or view a demo of app using this login: <br></br>
+								username: demo@demo.com <br></br>
+								password: demo <br></br>
+								<a target="_blank" rel="noopener noreferrer" href="https://github.com/sophiedamelio/project_4" className="projectCta" style={{ marginLeft: "0vh" }}>Link to the Repo</a>
+								<a target="_blank" rel="noopener noreferrer" href="https://songspace-app.herokuapp.com/" className="projectCta" style={{ marginLeft: "0vh" }}>Explore songSpace</a>
+							</Card.Description>
+						</Card.Content>
+					</Card>
+				</Grid.Column>
 
-					<Grid.Column>
-						<Card style={{ backgroundColor: "rgb(234, 204, 203)", width: "90%", height: "100%", marginLeft: "0vh", marginRight: "5vh" }}>
-							<Card.Content>
-								<Card.Header style={{ color: "rgb(110, 87, 91)", fontFamily: "OPTIAmway" }}>everyJob Django App</Card.Header>
-								<Image src="https://i.imgur.com/ixSrCHd.png" alt="screenshot of app" size={'big'} style={{ paddingTop: "2vh", paddingBottom: "2vh" }} />
-								<Card.Description style={{ color: "rgb(110, 87, 91)" }}>
-									<p class="card-text">As a part of a four person team I collaboratively  built a Fullstack Django app with full CRUD functionality.
-										We utilized PostgreSQL for database storage, Django built-in Authentication, and deployment on Heroku.</p>
-									<a className="projectCta" target="_blank" rel="noopener noreferrer" href="https://github.com/sophiedamelio/everyJob" style={{ marginLeft: "0vh" }}>Link to the Repo</a>
-									<a className="projectCta" target="_blank" rel="noopener noreferrer" href="https://sophie-everyjob.herokuapp.com/" style={{ marginLeft: "0vh" }}>Explore everyJob</a>
-								</Card.Description>
-							</Card.Content>
-						</Card>
-					</Grid.Column>
-				</Grid.Row>
+				<Grid.Column>
+					<Card style={{ backgroundColor: "rgb(234, 204, 203)", width: "90%", height: "100%", marginLeft: "0vh", marginRight: "5vh" }}>
+						<Card.Content>
+							<Card.Header style={{ color: "rgb(110, 87, 91)", fontFamily: "OPTIAmway" }}>everyJob Django App</Card.Header>
+							<Image src="https://i.imgur.com/ixSrCHd.png" alt="screenshot of app" size={'big'} style={{ paddingTop: "2vh", paddingBottom: "2vh" }} />
+							<Card.Description style={{ color: "rgb(110, 87, 91)" }}>
+								<p class="card-text">As a part of a four person team I collaboratively  built a Fullstack Django app with full CRUD functionality.
+									We utilized PostgreSQL for database storage, Django built-in Authentication, and deployment on Heroku.</p>
+								<a className="projectCta" target="_blank" rel="noopener noreferrer" href="https://github.com/sophiedamelio/everyJob" style={{ marginLeft: "0vh" }}>Link to the Repo</a>
+								<a className="projectCta" target="_blank" rel="noopener noreferrer" href="https://sophie-everyjob.herokuapp.com/" style={{ marginLeft: "0vh" }}>Explore everyJob</a>
+							</Card.Description>
+						</Card.Content>
+					</Card>
+				</Grid.Column>
+				{/*</Grid.Row>*/}
 
-				<Grid.Row>
-					<Grid.Column>
-						<Card style={{ backgroundColor: "rgb(234, 204, 203)", width: "90%", height: "100%", marginLeft: "5vh", marginRight: "0vh" }}>
-							<Card.Content>
-								<Card.Header style={{ color: "rgb(110, 87, 91)", fontFamily: "OPTIAmway" }}>Wishlist Express App</Card.Header>
-								<Image src="https://i.imgur.com/DgzgQqh.png" alt="screenshot of app" size={'big'} style={{ paddingTop: "2vh", paddingBottom: "2vh" }} />
-								<Card.Description style={{ color: "rgb(110, 87, 91)" }}>
-									<p class="card-text">I built a Wishlist app using Express with full CRUD functionality and Google OAuth.
-										MongoDB is used for storage, and it is deployed on Heroku.</p>
-									<a className="projectCta" target="_blank" rel="noopener noreferrer" href="https://github.com/sophiedamelio/project_two" style={{ marginLeft: "0vh" }}>Link to the Repo</a>
-									<a className="projectCta" target="_blank" rel="noopener noreferrer" href="https://sophie-project-two.herokuapp.com/" style={{ marginLeft: "0vh" }}>Get Listing</a>
-								</Card.Description>
-							</Card.Content>
-						</Card>
-					</Grid.Column>
+				{/*<Grid.Row>*/}
+				<Grid.Column>
+					<Card style={{ backgroundColor: "rgb(234, 204, 203)", width: "90%", height: "100%", marginLeft: "5vh", marginRight: "0vh" }}>
+						<Card.Content>
+							<Card.Header style={{ color: "rgb(110, 87, 91)", fontFamily: "OPTIAmway" }}>Wishlist Express App</Card.Header>
+							<Image src="https://i.imgur.com/DgzgQqh.png" alt="screenshot of app" size={'big'} style={{ paddingTop: "2vh", paddingBottom: "2vh" }} />
+							<Card.Description style={{ color: "rgb(110, 87, 91)" }}>
+								<p class="card-text">I built a Wishlist app using Express with full CRUD functionality and Google OAuth.
+									MongoDB is used for storage, and it is deployed on Heroku.</p>
+								<a className="projectCta" target="_blank" rel="noopener noreferrer" href="https://github.com/sophiedamelio/project_two" style={{ marginLeft: "0vh" }}>Link to the Repo</a>
+								<a className="projectCta" target="_blank" rel="noopener noreferrer" href="https://sophie-project-two.herokuapp.com/" style={{ marginLeft: "0vh" }}>Get Listing</a>
+							</Card.Description>
+						</Card.Content>
+					</Card>
+				</Grid.Column>
 
-					<Grid.Column>
-						<Card style={{ backgroundColor: "rgb(234, 204, 203)", width: "90%", height: "100%", marginLeft: "0vh", marginRight: "5vh" }}>
-							<Card.Content>
-								<Card.Header style={{ color: "rgb(110, 87, 91)", fontFamily: "OPTIAmway" }}>Connect Four Browser Game</Card.Header>
-								<Image src="https://i.imgur.com/b9LmOUu.png" alt="screenshot of app" size={'big'} style={{ paddingTop: "2vh", paddingBottom: "2vh" }} />
-								<Card.Description style={{ color: "rgb(110, 87, 91)" }}>
-									<p>I built a Connect Four browser game using JavaScript and HTML/CSS.
-										I used a Trello board to plan, and deployed via Github Pages. </p>
-									<a className="projectCta" target="_blank" rel="noopener noreferrer" href="https://github.com/sophiedamelio/connect_four/" style={{ marginLeft: "0vh" }}>Link to the Repo</a>
-									<a className="projectCta" target="_blank" rel="noopener noreferrer" href="https://sophiedamelio.github.io/connect_four/" style={{ marginLeft: "0vh" }}>Play Connect Four</a>
-								</Card.Description>
-							</Card.Content>
-						</Card>
-					</Grid.Column>
-				</Grid.Row>
+				<Grid.Column>
+					<Card style={{ backgroundColor: "rgb(234, 204, 203)", width: "90%", height: "100%", marginLeft: "0vh", marginRight: "5vh" }}>
+						<Card.Content>
+							<Card.Header style={{ color: "rgb(110, 87, 91)", fontFamily: "OPTIAmway" }}>Connect Four Browser Game</Card.Header>
+							<Image src="https://i.imgur.com/b9LmOUu.png" alt="screenshot of app" size={'big'} style={{ paddingTop: "2vh", paddingBottom: "2vh" }} />
+							<Card.Description style={{ color: "rgb(110, 87, 91)" }}>
+								<p>I built a Connect Four browser game using JavaScript and HTML/CSS.
+									I used a Trello board to plan, and deployed via Github Pages. </p>
+								<a className="projectCta" target="_blank" rel="noopener noreferrer" href="https://github.com/sophiedamelio/connect_four/" style={{ marginLeft: "0vh" }}>Link to the Repo</a>
+								<a className="projectCta" target="_blank" rel="noopener noreferrer" href="https://sophiedamelio.github.io/connect_four/" style={{ marginLeft: "0vh" }}>Play Connect Four</a>
+							</Card.Description>
+						</Card.Content>
+					</Card>
+				</Grid.Column>
+				{/*</Grid.Row>*/}
 
-				<Grid.Row>
-					<Grid.Column>
-						<Card style={{ backgroundColor: "rgb(234, 204, 203)", width: "90%", height: "100%", marginLeft: "5vh", marginRight: "0vh" }}>
-							<Card.Content>
-								<Card.Header style={{ color: "rgb(110, 87, 91)", fontFamily: "OPTIAmway" }}>Ideas To Go Website</Card.Header>
-								<Image src="https://i.imgur.com/Zgn5lPV.jpg" alt="screenshot of app" size={'big'} style={{ paddingTop: "2vh", paddingBottom: "2vh" }} />
-								<Card.Description style={{ color: "rgb(110, 87, 91)" }}>
-									<p class="card-text">I developed and designed this website with provided design assets. I organized content, converted graphics and created page layouts. I wrote responsive CSS including section transitions, CTAs, and general styling of elements beyond the built in Squarespace options. </p>
-									<a className="projectCta" target="_blank" rel="noopener noreferrer" href='https://github.com/sophiedamelio/itg_css' style={{ marginLeft: "0vh" }}>Link to the Repo</a>
-									<a className="projectCta" href='/itg_detail' style={{ marginLeft: "0vh" }}>View</a>
-								</Card.Description>
-							</Card.Content>
-						</Card>
-					</Grid.Column>
-				</Grid.Row>
+				{/*<Grid.Row>*/}
+				<Grid.Column>
+					<Card style={{ backgroundColor: "rgb(234, 204, 203)", width: "90%", height: "100%", marginLeft: "5vh", marginRight: "0vh" }}>
+						<Card.Content>
+							<Card.Header style={{ color: "rgb(110, 87, 91)", fontFamily: "OPTIAmway" }}>Ideas To Go Website</Card.Header>
+							<Image src="https://i.imgur.com/Zgn5lPV.jpg" alt="screenshot of app" size={'big'} style={{ paddingTop: "2vh", paddingBottom: "2vh" }} />
+							<Card.Description style={{ color: "rgb(110, 87, 91)" }}>
+								<p class="card-text">I developed and designed this website with provided design assets. I organized content, converted graphics and created page layouts. I wrote responsive CSS including section transitions, CTAs, and general styling of elements beyond the built in Squarespace options. </p>
+								<a className="projectCta" target="_blank" rel="noopener noreferrer" href='https://github.com/sophiedamelio/itg_css' style={{ marginLeft: "0vh" }}>Link to the Repo</a>
+								<a className="projectCta" href='/itg_detail' style={{ marginLeft: "0vh" }}>View</a>
+							</Card.Description>
+						</Card.Content>
+					</Card>
+				</Grid.Column>
+				{/*</Grid.Row>*/}
 			</Grid>
 		</div>
 	)
