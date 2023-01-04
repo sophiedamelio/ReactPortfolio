b968a98cb1f2928c6fec842e813daba68c9ea3d4
sophiedamelio <sophiedamelio@gmail.com>
2023-01-04 13:35:14 -0800

    apply responsive prop to both itg photo grids

--- src/components/Itg/Itg.jsx
+++ src/components/Itg/Itg.jsx
@@ -25,7 +25,8 @@ export default function Itg() {
 				'View more pages',
 			//</div>,
 			content: [
-				<Grid columns={5}>
+				<Grid doubling columns={5}>
+					{/* add breakpoint for mobile, so only one image shows per row */}
 					<Grid.Row>
 						<Grid.Column>
 							<Image src="https://i.imgur.com/6dGoGb1.jpg" alt="Third slide" size={"medium"} />
@@ -100,8 +101,8 @@ export default function Itg() {
 				<h2 style={{ fontFamily: "OPTIAmway" }}>Ideas To Go Website Build</h2>
 				<p>I developed and designed this website for Ideas To Go from May 2021 - Oct 2021 as a freelance contractor. I no longer maintain the website, the contract ended upon completion of the build. It is hosted on Squarespace, and uses a custom theme I created. I was provided design assets including logos, fonts, and a color pallet. I worked with their Designer <a href="https://francesbarra.wixsite.com/mysite" target="blank" style={{ color: "rgb(110, 87, 91)", textDecoration: "underline" }}>Frances Barra</a> to implement page designs, and picked up so many skills throughout our communication as well as technical exposure. I learned a lot from the task of recreating detailed designs for the live website.</p>
 				<p>I developed the entire website of 40+ pages, and utilized a combination of built in Squarespace features and my own custom CSS. The process of this project brought me many new ways of approaching a problem, and I learned the most from utilizing flexibility and researching and understanding process before delving deep into it. The most technical aspects of the website that I implemented are the angled and responsive page transitions, the custom CTA buttons, the contact form (embed from their CRM), and the responsive images.</p>
-				{/*<div class="carousel-item active">*/}
-				<Grid columns={5}>
+				{/* add breakpoint for mobile, so only one image shows per row */}
+				<Grid doubling columns={5}>
 					<Grid.Row>
 						<Grid.Column>
 							<Image src="https://i.imgur.com/gK6tkHZ.jpg" alt="First slide" size={"medium"} />
