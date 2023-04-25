14fb630c27d565686023c6dc3f71d741efa844ea
sophiedamelio <sophiedamelio@gmail.com>
2023-04-24 20:23:46 -0700

    ga photo mobile stying complete

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,6 +4,9 @@ import { Grid, Image } from 'semantic-ui-react'
 export default function About() {
 	return (
 		<div id="aboutSection">
+			<head>
+				<meta name="viewport" content="width=device-width" />
+			</head>
 			<Grid columns={2}>
 				<h2 className="aboutTitles">About</h2>
 				<Grid.Row>
@@ -27,18 +30,18 @@ export default function About() {
 			</Grid>
 			<Grid columns={2}>
 				<h2 className="aboutTitles" style={{ paddingBottom: "1rem" }}>Certifications</h2>
-				<Grid.Row verticalAlign={"middle"}>
-					<Grid.Column style={{ width: "10%", display: "flex" }} >
-						<Image src="https://i.imgur.com/hN7GAid.png" alt="general-assembly-logo" size="tiny" />
+				<Grid.Row verticalAlign={"middle"} style={{ display: "flex", flexWrap: "nowrap" }}>
+					<Grid.Column className="gaImageParent">
+						<Image className="gaImage" src="https://i.imgur.com/hN7GAid.png" alt="general-assembly-logo" />
 					</Grid.Column>
 					<Grid.Column style={{ width: "90%" }}>
 						<p>Completed a full-stack software engineering immersive program in an intensive, twelve-week, 420+ hour program focused on product development fundamentals, object-oriented programming, MVC frameworks, data modeling, and team collaboration strategies. Developed a portfolio of individual and group projects.</p>
 						<a href="https://drive.google.com/file/d/1vFMhW7A67_FyFyjmjjXrg0Gu0-61xG27/view" target="blank" className="aboutCta">Show credential</a>
 					</Grid.Column>
 				</Grid.Row>
-				<Grid.Row verticalAlign={"middle"} style={{ marginTop: "3vh", marginBottom: "3vh" }}>
-					<Grid.Column style={{ width: "10%", display: "flex" }}>
-						<Image src="https://i.imgur.com/5feNx95.png" alt="amazon-web-services-certification-image" size="tiny" />
+				<Grid.Row verticalAlign={"middle"} style={{ display: "flex", flexWrap: "nowrap" }}>
+					<Grid.Column className="awsImageParent" style={{ width: "10%" }}>
+						<Image className="awsImage" src="https://i.imgur.com/5feNx95.png" alt="amazon-web-services-certification-image" />
 					</Grid.Column>
 					<Grid.Column style={{ width: "90%" }}>
 						<p>Earned Amazon Web Services Cloud Practitioner in December 2022.</p>
