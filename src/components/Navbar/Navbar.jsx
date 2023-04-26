a84b8c486ebe68b712050bab3bc83eb428589cef
sophiedamelio <sophiedamelio@gmail.com>
2023-04-25 21:53:27 -0700

    begin tab functionality for nav menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,46 @@
 import "./Navbar.css"
-import { Header } from 'semantic-ui-react';
+import { Header, Menu } from 'semantic-ui-react';
+import React, { useState } from "react";
 
 
 export default function Navbar() {
+
+	const [activeItem, setActiveItem] = useState({ activeItem: 'bio' });
+
+	function handleItemClick(e, tabName) {
+		// set active tab to be a color for testing here?
+		setActiveItem({ activeItem: tabName });
+	}
+
+	//const { activeItem } = this.state
+	//activeItem = activeItem;
+
+	//const { activeItem } = this.state
+
 	return (
 		//<Header id="all-nav-links">
-		<div id="all-nav-links">
-			<a href="/portfolio" id="title">Sophie Damelio</a>
-			<div id="menu-nav-links">
-				<a href="/about" className="nav-link">About</a>
-				<a href="/projects" className="nav-link">Projects</a>
-				<a href="/contact" className="nav-link">Contact</a>
+		<>
+			<Menu tabular>
+				<Menu.Item
+					name='bio'
+					active={activeItem === 'bio'}
+					onClick={handleItemClick}
+				/>
+				<Menu.Item
+					name='photos'
+					active={activeItem === 'photos'}
+					onClick={handleItemClick}
+				/>
+			</Menu>
+			<div id="all-nav-links">
+				<a href="/portfolio" id="title">Sophie Damelio</a>
+				<div id="menu-nav-links">
+					<a href="/about" className="nav-link">About</a>
+					<a href="/projects" className="nav-link">Projects</a>
+					<a href="/contact" className="nav-link">Contact</a>
+				</div>
 			</div>
-		</div>
+		</>
 		//{/*</Header>*/ }
 	)
 }
