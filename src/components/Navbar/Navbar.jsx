fc93485b2db484e30a4106d7e766bfe1cef4ac43
sophiedamelio <sophiedamelio@gmail.com>
2023-05-01 15:09:27 -0700

    alignement on mobile done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,13 +12,13 @@ export default function Navbar() {
 	)
 
 	return (
-		<>
+		<div id="navSection">
 			<a href="/" id="title">Sophie Damelio</a>
 			<div id="all-nav-links">
 				<div id="menu-nav-links">
 					<SmoothScroll id="content" />
 				</div>
 			</div>
-		</>
+		</div>
 	)
 }
