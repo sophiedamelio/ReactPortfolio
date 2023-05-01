e476ef49f399af77b31de7c3b337e22afe25cb5f
sophiedamelio <sophiedamelio@gmail.com>
2023-05-01 14:12:36 -0700

    align components on itg detail page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,19 +4,21 @@ import { Grid } from "semantic-ui-react"
 export default function Footer() {
 	return (
 		<section id="contact">
-			<Grid columns={1} style={{ margin: "1rem", padding: "3vh", width: "100%" }}>
-				<Grid.Row>
-					<Grid.Column>
-						<h2 style={{ fontFamily: "OPTIAmway" }}>Contact</h2>
-						<p>Email: sophiedamelio@gmail.com</p>
-						<div style={{ marginTop: "3.5vh" }}>
-							<a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/sophiedamelio/" className="footer-nav-link">LinkedIn</a>
-							<a target="_blank" rel="noopener noreferrer" href="https://github.com/sophiedamelio/" className="footer-nav-link">GitHub</a>
-							<a href="https://docs.google.com/document/d/1LPEHmH7vYy471BvStVojWTBq3CVGEgukQoUej6Y2k0w/export?format=pdf" download="sophie_damelio_resume" className="footer-nav-link">Download Resume</a>
-							<a href="https://docs.google.com/document/d/1LPEHmH7vYy471BvStVojWTBq3CVGEgukQoUej6Y2k0w/export?format=pdf" className="footer-nav-link">View Resume?</a>
-						</div>
-					</Grid.Column>
-				</Grid.Row>
+			<Grid columns={1} style={{ width: "100%" }}>
+				<div style={{ margin: "5vh" }}>
+					<Grid.Row>
+						<Grid.Column>
+							<h2 style={{ fontFamily: "OPTIAmway" }}>Contact</h2>
+							<p>Email: sophiedamelio@gmail.com</p>
+							<div style={{ marginTop: "3.5vh" }}>
+								<a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/sophiedamelio/" className="footer-nav-link">LinkedIn</a>
+								<a target="_blank" rel="noopener noreferrer" href="https://github.com/sophiedamelio/" className="footer-nav-link">GitHub</a>
+								<a href="https://docs.google.com/document/d/1LPEHmH7vYy471BvStVojWTBq3CVGEgukQoUej6Y2k0w/export?format=pdf" download="sophie_damelio_resume" className="footer-nav-link">Download Resume</a>
+								<a href="https://docs.google.com/document/d/1LPEHmH7vYy471BvStVojWTBq3CVGEgukQoUej6Y2k0w/export?format=pdf" className="footer-nav-link">View Resume?</a>
+							</div>
+						</Grid.Column>
+					</Grid.Row>
+				</div>
 			</Grid>
 		</section>
 	)
