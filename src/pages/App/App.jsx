4844a444b9685e964df50c5869de0e901ace224e
sophiedamelio <sophiedamelio@gmail.com>
2023-10-04 20:47:12 -0700

    add to do list in app.jsx

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,8 +1,14 @@
 import './App.css';
-import HomePage from '../HomePage/HomePage'
-import ItgDetail from '../ItgDetail/ItgDetail';
+//import HomePage from '../HomePage/HomePage'
+//import ItgDetail from '../ItgDetail/ItgDetail';
 
 import { Routes, Route } from "react-router-dom";
+
+
+//to do:
+// edit the view resume link to be the correct resume
+//from 610 pixels and below, media query and fix spacing when resizing smaller whole navbar
+
 
 function App() {
   return (
